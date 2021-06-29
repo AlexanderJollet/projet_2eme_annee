7c02b4e504e3f80d5b12c079ef3703616e7ca513
AlexanderJollet <alexander.jollet3@gmail.com>
2021-06-29 16:26:28 +0200

    add login+register

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -18,7 +18,7 @@ class IndexController extends AbstractController
 {
 
     /**
-     * @Route("/", name="vehicule_index", methods={"GET"})
+     * @Route("/home", name="vehicule_index", methods={"GET"})
      */
     public function index(VehiculeRepository $vehiculeRepository): Response
     {
@@ -26,31 +26,4 @@ class IndexController extends AbstractController
             'vehicules' => $vehiculeRepository->findAll(),
         ]);
     }
-
-
-   /**
-      * @Route("/vehicule/save")
-      */
-     public function save() {
-       $entityManager = $this->getDoctrine()->getManager();
-
-       $vehicule = new Vehicule();
-       $vehicule->setType('Scooter');
-       $vehicule->setMarque("BMW");
-       $vehicule->setModele("2");
-       $vehicule->setNumeroSerie("dezcn1342");
-       $vehicule->setCouleur("blanc");
-       $vehicule->setPLaqueImmatriculation("12-BMW-34");
-       $vehicule->setNbKilometre(12045);
-       $vehicule->setDateAchat('2012-09-01');
-       $vehicule->setPrixAchat(19999);
-       $vehicule->setDureeLocation(2);
-
-      
-       $entityManager->persist($vehicule);
-       $entityManager->flush();
-
-       return new Response('Article enregisté avec id   '.$vehicule->getId());
-        }
-
-    }
+}
